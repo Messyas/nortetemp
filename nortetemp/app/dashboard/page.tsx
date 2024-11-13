68dd7a0c7f3e3ed810f819a0a76b3b3ff299da1e
Lucas Medeiros <2022001198@ifam.edu.br>
2024-11-12 23:45:38 -0400

    Consumo da API

--- nortetemp/app/dashboard/page.tsx
+++ nortetemp/app/dashboard/page.tsx
@@ -1,3 +1,36 @@
-export default function Page() {
-    return <p>Pagina do DashBoard</p>;
+"use client";
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function Home() {
+  const [forecastData, setForecastData] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await axios.get('/api/weather');
+        console.log("Resposta da API:", response.data); // Log da resposta completa
+        setForecastData(response.data.DailyForecasts);
+      } catch (error) {
+        console.error("Erro ao carregar os dados:", error);
+      }
+    };
+    fetchWeatherData();
+  }, []);
+
+  if (!forecastData) return <p>Carregando...</p>;
+
+  return (
+    <div>
+      <h1>Previsão de Tempestades para 5 Dias</h1>
+      {forecastData.map((day, index) => (
+        <div key={index}>
+          <h2>{new Date(day.Date).toLocaleDateString()}</h2>
+          <p>Probabilidade de Tempestade: {day.Day.ThunderstormProbability}%</p>
+          <p>Descrição: {day.Day.IconPhrase}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
